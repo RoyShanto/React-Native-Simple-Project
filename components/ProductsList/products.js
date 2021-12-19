78314b52390abe1b3e571f37c107d41c48b46d56
Your Name <roycse17@gmail.com>
2021-12-19 23:53:17 +0600

    first commit

---
+++ components/ProductsList/products.js
@@ -0,0 +1,22 @@
+export default [{
+    name: 'Nikon',
+    tagline: 'This is best for photo',
+    taglineCTA: 'Price : $200',
+    image: require('../../assets/images/2.png'),
+  }, {
+    name: 'Cannon',
+    tagline: 'This is best for video',
+    taglineCTA: 'Price : $200',
+    image: require('../../assets/images/1.png'),
+  }, {
+    name: 'Nikon Lense',
+    tagline: 'It is good for photo and video',
+    taglineCTA: 'Price : $200',
+    image: require('../../assets/images/4.png'),
+  }, {
+    name: 'Cannon Lense',
+    tagline: 'This camera also have audio features',
+    taglineCTA: 'Price : $200',
+    image: require('../../assets/images/3.png'),
+  }];
+  
